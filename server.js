5f270375c0cb4512d4a77f41950eb3bddcda1564
unknown <mriduldey94@gmail.com>
2021-03-11 23:28:19 +0530

    hardcoded data from post api call to add in mongoDB, is removed

--- server.js
+++ server.js
@@ -26,24 +26,8 @@ app.get("/", (req, res) => {
 });
 
 app.post("/tinder/card", (req, res) => {
-  //   const dbCard = req.body;
-  const dbCard = [
-    {
-      name: "Kajal",
-      imgUrl:
-        "https://i.pinimg.com/736x/83/af/4f/83af4f76078a188d3b1aa551401b8c76.jpg",
-    },
-    {
-      name: "Shahrukh",
-      imgUrl:
-        "https://i.pinimg.com/originals/9f/8b/3f/9f8b3f486073f5b29d5d08bcadaf64b1.jpg",
-    },
-    {
-      name: "Martin",
-      imgUrl:
-        "https://www.scienceabc.com/wp-content/uploads/2016/01/shutterstock_338992685.jpg",
-    },
-  ];
+  const dbCard = req.body;
+
   Cards.create(dbCard, (err, data) => {
     if (err) {
       res.status(500).send(err);
